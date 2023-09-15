4d46674993d45636d14ebcf65a98b5b31021c648
Heba-Webdev23 <jtvonline@yahoo.com>
2023-09-15 19:39:02 +0300

    Modified the component that fetches a movie by its id

--- app/api/getMovieById.js
+++ app/api/getMovieById.js
@@ -1,10 +1,13 @@
 const BASE_URL = "https://api.themoviedb.org/3/movie/";
-const fetchMovieById = async (id) => {
+const getMovieById = async (id) => {
   const fetchData = await fetch(
     `${BASE_URL}${id}?api_key=${process.env.NEXT_PUBLIC_API_KEY}`
   );
   const res = await fetchData.json();
+  if (res.success === false) {
+    throw new Error(res.status_message);
+  }
   return res;
 };
 
-module.exports = fetchMovieById;
+module.exports = getMovieById;
