fc93cbeb8f0ee7c9d1bee6bd1a861365f5858f0f
Heba-Webdev23 <jtvonline@yahoo.com>
2023-09-15 19:45:45 +0300

    Added the lading component to be shown to users while data is being fetched

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -2,12 +2,14 @@
 import HeroNav from "./HeroNav";
 import { useEffect, useState } from "react";
 import HeroMovie from "./HeroMovie";
+import HeroLoadingSkeleton from "./HeroLoadingSkeleton";
 
-export default function Hero() {
+export default function Hero({ setIsLoading }) {
   const [heroMovie, setHeroMovie] = useState({
     title: "",
     overview: "",
     averageVote: "",
+    id: "",
   });
   const apiKey = process.env.NEXT_PUBLIC_API_KEY;
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
@@ -20,13 +22,13 @@ export default function Hero() {
     )
       .then((data) => data.json())
       .then((res) => {
-        console.log(res.results);
         const posterUrl = `${imageBaseUrl}/original/${res.results[13].poster_path}`;
         setPoster(posterUrl);
         setHeroMovie({
-          title: res.results[13].title,
-          overview: res.results[13].overview,
-          averageVote: res.results[13].vote_average,
+          title: res?.results[13]?.title,
+          overview: res?.results[13]?.overview,
+          averageVote: res?.results[13]?.vote_average,
+          id: res?.results[13]?.id,
         });
       });
   };
@@ -37,22 +39,28 @@ export default function Hero() {
   }, []);
 
   return (
-    <header
-      style={{
-        "--image-url": `url(${poster})`,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-      }}
-      className="bg-blue-400 py-10 bg-[image:var(--image-url)] drop-shadow-2xl relative"
-    >
-      <HeroNav />
-      <HeroMovie
-        title={heroMovie.title}
-        overview={heroMovie.overview}
-        averageVote={heroMovie.averageVote}
-      />
-      <div className="w-[100%] h-[100%] absolute bg-gray-900 top-0 opacity-[0.7] z-0"></div>
-    </header>
+    <>
+      {heroMovie.title && poster && (
+        <header
+          style={{
+            "--image-url": `url(${poster})`,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+          }}
+          className="bg-blue-400 py-10 bg-[image:var(--image-url)] drop-shadow-2xl relative"
+        >
+          <HeroNav />
+          <HeroMovie
+            title={heroMovie?.title}
+            overview={heroMovie?.overview}
+            averageVote={heroMovie?.averageVote}
+            id={heroMovie?.id}
+          />
+          <div className="w-[100%] h-[100%] absolute bg-gray-900 top-0 opacity-[0.7] z-0"></div>
+        </header>
+      )}
+      {!heroMovie.title && <HeroLoadingSkeleton />}
+    </>
   );
 }
