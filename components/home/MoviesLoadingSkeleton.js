5879cd5ae96533a3f22ee23e49a64bd60d0d79ea
Heba-Webdev23 <jtvonline@yahoo.com>
2023-09-15 19:48:44 +0300

    Created a loading component to be shown to users while movies are being fetched

---
+++ components/home/MoviesLoadingSkeleton.js
@@ -0,0 +1,14 @@
+export default function MoviesLoadingSkeleton() {
+  return (
+    <section className="container-custom mt-8 cards">
+      <div className="container-custom bg-gray-300 w-[100%] h-[65vh] mt-2 flex flex-col gap-4 skeleton"></div>
+      <div className="container-custom bg-gray-300 w-[100%] h-[65vh] mt-2 flex flex-col gap-4 skeleton"></div>
+      <div className="container-custom bg-gray-300 w-[100%] h-[65vh] mt-2 flex flex-col gap-4 skeleton"></div>
+      <div className="container-custom bg-gray-300 w-[100%] h-[65vh] mt-2 flex flex-col gap-4 skeleton"></div>
+      <div className="container-custom bg-gray-300 w-[100%] h-[65vh] mt-2 flex flex-col gap-4 skeleton"></div>
+      <div className="container-custom bg-gray-300 w-[100%] h-[65vh] mt-2 flex flex-col gap-4 skeleton"></div>
+      <div className="container-custom bg-gray-300 w-[100%] h-[65vh] mt-2 flex flex-col gap-4 skeleton"></div>
+      <div className="container-custom bg-gray-300 w-[100%] h-[65vh] mt-2 flex flex-col gap-4 skeleton"></div>
+    </section>
+  );
+}
